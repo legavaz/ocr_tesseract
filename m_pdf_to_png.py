b03d02cc661f0d3a23db083a5735793f5378435d
dvi <legavaz@gmail.com>
2020-08-20 13:52:41 +0300

    lp

--- m_pdf_to_png.py
+++ m_pdf_to_png.py
@@ -1,34 +1,29 @@
 ﻿import sys
 import subprocess
-from pdf2image import convert_from_path, convert_from_bytes
+from pdf2image import convert_from_path
 
 # Для Windows
-def png_to_txt(file_name):    
-    out_put     =   'out\\file_txt'
-    lang        =   '-l rus+eng'   
-    command     =   'tesseract.exe {0} {1} {2}'.format(file_name,out_put,lang)
-    print('COMMAND: ',command)
+
+def png_to_txt(file_name,path):
+    out_put = path+'\\file_txt'
+    lang = '-l rus+eng'
+    command = 'tesseract.exe {0} {1} {2}'.format(file_name, out_put, lang)
+    print('COMMAND: ', command)
     subprocess.call(command)
 
-def ocr(filename,path):
+
+def ocr(filename, path):
     images = convert_from_path(filename)
-    i = 0    
+    i = 0
     for target_list in images:
         i = +1
-        f_name  =   path+'\\'+'file_{0}.png'.format(i)
-        target_list.save(f_name) 
-        png_to_txt(f_name)
+        f_name = path+'\\'+'file_{0}.png'.format(i)
+        target_list.save(f_name)
+        png_to_txt(f_name,path)
 
 
-file_in     =   r'D:\Py\ocr\sch.pdf'
-out_path    =   r'D:\Py\ocr\out'
+# main
+file_in 	= r'C:\Users\dvi\Desktop\Договор ФА000134 от 08.05.2020.pdf'
+out_path 	= r'C:\Users\dvi\AppData\Local\Temp\out'
 
-ocr(file_in,out_path)
-
-
-# if __name__=='__main__':
-#     if len(sys.argv)==3:
-#         print('конвертация:{0} -> {1}'.format(sys.argv[1],sys.argv[2]))
-#         array   =   convert_save(sys.argv[1],sys.argv[2])
-#     else:
-#         print('arg1 - in pdf,arg2 - out path')
+ocr(file_in, out_path)
