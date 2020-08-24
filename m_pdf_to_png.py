896a74653bfaf5960f927c00006f72bd1f7bcfe9
dvi <legavaz@gmail.com>
2020-08-24 13:13:25 +0300

    lp

--- m_pdf_to_png.py
+++ m_pdf_to_png.py
@@ -1,11 +1,20 @@
-﻿import sys
+﻿import sys,os
 import subprocess
 from pdf2image import convert_from_path
 
 # Для Windows
 
+def return_name(file_name):
+    f_n =   'file_name'
+    arr_f_n =   os.path.basename(file_name).split('.')
+    if len(arr_f_n)>0:
+        f_n =   arr_f_n[0]
+        f_n =   f_n.replace(' ','_')
+
+    return f_n
+
 def png_to_txt(file_name,path):
-    out_put = path+'\\file_txt'
+    out_put = path+'\\'+return_name(file_name)
     lang = '-l rus+eng'
     command = 'tesseract.exe {0} {1} {2}'.format(file_name, out_put, lang)
     print('COMMAND: ', command)
@@ -15,9 +24,10 @@ def png_to_txt(file_name,path):
 def ocr(filename, path):
     images = convert_from_path(filename)
     i = 0
+    f_n = return_name(filename)
     for target_list in images:
-        i = +1
-        f_name = path+'\\'+'file_{0}.png'.format(i)
+        i = i+1
+        f_name = path+'\\'+f_n+'_{0}.png'.format(i)
         target_list.save(f_name)
         png_to_txt(f_name,path)
 
@@ -25,5 +35,7 @@ def ocr(filename, path):
 # main
 file_in 	= r'C:\Users\dvi\Desktop\Договор ФА000134 от 08.05.2020.pdf'
 out_path 	= r'C:\Users\dvi\AppData\Local\Temp\out'
+
+# print(return_name(file_in))
 
 ocr(file_in, out_path)
